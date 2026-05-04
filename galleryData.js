f3075f6d524184c7f607befc8110cb3258568523
KaiTel <yosefnt1@gmail.com>
2026-05-04 15:56:12 +0300

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -10,19 +10,17 @@ const galleryData = {
         }
     ],
 
-endMessage: `Happy Birthday to the most beautiful soul I know 
+const endMessage = `Happy Birthday to the most beautiful soul I know 
 
-I honestly don’t even know where to start, because no words will ever be enough to describe how much you mean to me. You’re not just my friend… you’re my safe place, my happiness, and one of the best things that ever happened in my life.
+I honestly don’t even know where to start...
 
-Through every laugh, every late-night conversation, every little moment — you’ve been there. You understand me in a way no one else does, and I’m so grateful for every memory we’ve created together. And the best part? We’re just getting started.
+Through every laugh...
 
-I hope this year brings you everything your heart has been wishing for — success, love, peace, and endless happiness. You deserve the world and so much more, and I truly hope life surprises you with the most beautiful things.
+I hope this year brings you everything...
 
-No matter where life takes us, I’ll always be right here for you. Supporting you, laughing with you, and standing by your side through everything.
+No matter where life takes us...
 
-Thank you for being you. Never change, because the way you are is already perfect to me.
-
-I love you more than words can ever explain. Happy Birthday again, my favorite person `,
+I love you more than words can ever explain`;
     endImage: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&"
 
 };
