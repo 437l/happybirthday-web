b07aa0089e8f209573ede66e94168793f32450af
KaiTel <yosefnt1@gmail.com>
2026-05-04 16:01:12 +0300

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -20,7 +20,7 @@ I hope this year brings you everything...
 
 No matter where life takes us...
 
-I love you more than words can ever explain`;
+I love you more than words can ever explain`,
     endImage: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&"
 
 };
