5b7e48d04decf558f844990ada26a6f063fb76c3
KaiTel <yosefnt1@gmail.com>
2026-05-04 16:15:34 +0300

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -12,6 +12,6 @@ const galleryData = {
 
     endMessage: `the end message hfffff `,
 
-    endImage: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&"
+    endImage: "https://cdn.discordapp.com/attachments/1500807973042257941/1500848219398340729/IMG_4969.jpg?ex=69f9ed53&is=69f89bd3&hm=06b562a208a246d690dfd818f942896ec8d786c0f51c6ab9e12e877680cd7c74&"
 
 };
