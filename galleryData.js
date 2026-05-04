c938705f0a4162b0421e3586191ab1d758969b0b
437l <yosofnt@gmailcom>
2026-05-04 15:14:45 +0300

    first commit

---
+++ galleryData.js
@@ -0,0 +1,16 @@
+const galleryData = {
+
+    welcomeText: "Happy birthday, Mia!",
+
+    people: [
+        {
+            name: "kaitel",
+            image: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&",
+            caption: "كل سنة وانتي طيبة"
+        }
+    ],
+
+    endMessage: "20 years we never meet someone like you we with you in every moment",
+    endImage: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&"
+
+};
