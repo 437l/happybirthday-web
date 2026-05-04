758d016c900893216deec5a29c1954906c63a2a9
KaiTel <yosefnt1@gmail.com>
2026-05-04 16:08:09 +0300

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -4,7 +4,7 @@ const galleryData = {
 
     people: [
         {
-            name: "kaitel",
+            name: "KaiTel",
             image: "https://cdn.discordapp.com/attachments/1500807973042257941/1500817364747419801/IMG_4965.jpg?ex=69f9d097&is=69f87f17&hm=d35290941eda1dfabc127b745e3811b3abdb5a0c310f4ef655c5cb10c6f9479a&",
             caption: "كل سنة وانتي طيبة"
         }
